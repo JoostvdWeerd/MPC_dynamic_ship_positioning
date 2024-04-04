58f4909f9d2699ebc399983a904c6e738fc86bc5
JoostvdWeerd <j.vanderweerd@student.tudelft.nl>
2024-04-04 16:44:56 +0200

    Het werkt een soort vvan

--- Model.py
+++ Model.py
@@ -9,8 +9,9 @@ class Model:
     b: B matrix LTI system
     c: C matrix LTI system
     p: prediction horizon
+    dt: timestep
     """
-    def __init__(self, x0, a, b, c, p):
+    def __init__(self, x0, a, b, c, p, dt):
         self.current_state = x0
         self.current_time_step = 0
         self.states = []
@@ -18,6 +19,7 @@ class Model:
         self.B = b
         self.C = c
         self.p = p
+        self.dt = dt
     #     self.O = self.gen_o_matrix(p)
     #     self.M = self.gen_m_matrix(p)
     #
@@ -45,24 +47,23 @@ class Model:
 
     def calc_prediction(self, u):
         x_k = self.current_state
-        prediction = []
+        prediction = np.zeros((self.A.shape[0], self.p))
         for i in range(self.p):
-            x_kp1 = self.calc_next_state(x_k, u)
-            prediction.append(x_kp1)
+            prediction[:, i:i+1] = x_k
+            x_kp1 = self.calculate_next_state(x_k, u[:, i:i+1])
             x_k = x_kp1
+        return prediction
 
     def update_state(self, state):
         self.states.append(self.current_state)
         self.current_state = state
 
     def calculate_next_state(self, x, u):
-        self.A[0, 3] = np.cos(x[2])
-        self.A[0, 4] = np.sin(-x[2])
-        self.A[1, 3] = np.sin(x[2])
-        self.A[1, 4] = np.cos(x[2])
+        self.A[0, 3] = np.round(np.cos(x[2]), 10)
+        self.A[0, 4] = -np.round(np.sin(x[2]), 10)
+        self.A[1, 3] = np.round(np.sin(x[2]), 10)
+        self.A[1, 4] = np.round(np.cos(x[2]), 10)
         self.A[2, 5] = 1
-        return self.A @ x + self.B @ u
-
-    def state_prediction(self):
-        return self.O @ self.current_state
-
+        x_kp = x + self.A @ x * self.dt + self.B @ u * self.dt
+        x_kp[2, 0] = np.mod(x_kp[2, 0], 2*np.pi)
+        return x_kp
--- main.py
+++ main.py
@@ -1,45 +1,111 @@
 import numpy as np
 from Model import Model
-
+from matplotlib import pyplot as plt
 
 ################################
 # TEST model
 ################################
 n = 6  # number of states
-m = 1  # number of inputs
-l = 2  # number of outputs
+m = 4  # number of inputs (Thrusters)
+l = 3  # number of outputs
+
+mass = 500         # temp
+X_u = 100             # temp
+Y_v = 100         # temp
+N_r = 100       # temp
+Y_r = 10       # temp
+N_v = Y_r           # assumption starboard and port symmetries
+X_udot = 100       # temp
+Y_vdot = 100      # temp
+N_rdot = 100       # temp
+Y_rdot = 10       # temp
+N_vdot = Y_rdot   # assumption starboard and port symmetries
+L = 10            # Length
+B = 2             # width
+T = 1             # depth
+rho = 1        # density of water
+I_zz = np.pi*rho*L*B*T*(B**2 + L**2)/120
+thrusters = [(5, 0, np.pi/2), (5, 0, -np.pi/2), (-0.8*L/2, -B/2, 0), (-0.8*L/2, B/2, 0)]
+
+# Mass matrix
+M = np.zeros((3, 3))
+M[0, 0] = mass + X_udot
+M[1, 1] = mass + Y_vdot
+M[1, 2] = Y_rdot
+M[2, 1] = N_vdot
+M[2, 2] = I_zz + N_rdot
+
+D = np.zeros((3, 3))
+D[0, 0] = X_u
+D[1, 1] = Y_vdot
+D[1, 2] = Y_r
+D[2, 1] = N_v
+D[2, 2] = N_r
+
+# Initialize state
+x0 = np.zeros((n, 1))
+x0[2, 0] = np.pi/2
 
 A = np.zeros((n, n))
-A[0, 1] = 1
-A[2, 3] = 1
-
-A[1, 0] = -2
-A[1, 1] = -2
-A[1, 2] = -1
-A[1, 3] = -1
-
-A[3, 0] = 1
-A[3, 1] = 1
-A[3, 2] = -1
-A[3, 3] = -1
-
+A[0, 3] = np.round(np.cos(x0[2]), 10)
+A[0, 4] = -np.round(np.sin(x0[2]), 10)
+A[1, 3] = np.round(np.sin(x0[2]), 10)
+A[1, 4] = np.round(np.cos(x0[2]), 10)
+A[2, 5] = 1
+A[3:6, 3:6] = - np.linalg.inv(M) @ D
 
 B = np.zeros((n, m))
-B[3, 0] = 0.1
+T_thrusters = np.zeros((3, m))
+for i, thruster in enumerate(thrusters):
+    L_N, L_E, alpha = thruster
+    T_thrusters[0, i] = np.round(np.cos(alpha), 10)
+    T_thrusters[1, i] = np.round(np.sin(alpha), 10)
+    T_thrusters[2, i] = -L_E*np.cos(alpha) + L_N*np.sin(alpha)
 
+B[3:6, :] = np.linalg.inv(M) @ T_thrusters
 C = np.zeros((l, n))
 C[0, 0] = 1
-x0 = np.ones((n, 1))
-# print(A @ x0)
-# print(x0)
-#
-# print(A)
-# print(B)
-# print(C)
-boat_model = Model(x0, A, B, C, 9)
-print(C)
-print(boat_model.state_prediction())
-# print(boat_model.O)
-# print(boat_model.M)
+C[1, 1] = 1
+C[2, 2] = 1
 
-# boat_model.calculate_next_state(boat_model.current_state, np.zeros((1, 1)))
+dt = 1
+prediction_horizon = 500
+u = np.zeros((4, prediction_horizon))
+
+u[0, :] = 0
+u[1, :] = 0
+u[2, :] = 0
+u[3, :] = -10
+boat_model = Model(x0, A, B, C, prediction_horizon, dt)
+prediction = boat_model.calc_prediction(u)
+
+plt.figure()
+plt.quiver(prediction[1, :], prediction[0, :], np.sin(prediction[2, :]), np.cos(prediction[2, :]), scale=50)
+plt.show()
+
+# plt.plot(prediction[1, :], prediction[0, :])
+# plt.title("Location N, E over time")
+# plt.show()
+#
+# plt.plot(prediction[2, :])
+# plt.show()
+#
+plt.plot(prediction[3, :])
+plt.title("Forward speed over time")
+plt.show()
+#
+plt.plot(prediction[4, :])
+plt.title("Speed to the side")
+plt.show()
+#
+plt.figure()
+plt.plot(prediction[2, :])
+plt.title("orientation")
+plt.show()
+
+plt.figure()
+plt.plot(prediction[5, :])
+plt.title("Rotational speed around centre")
+plt.show()
+
+
